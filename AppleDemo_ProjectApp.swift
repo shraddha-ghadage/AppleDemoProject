95273b4e9b4e8f30bcc97c7fd0099edfda221cd4
shraddha-ghadage <shrghadage@gmail.com>
2025-08-18 12:32:27 +0530

    assets added

---
+++ AppleDemo_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleDemo_ProjectApp.swift
+//  AppleDemo_Project
+//
+//  Created by JustMac on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleDemo_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+           // FrameworkGridView()
+        }
+    }
+}
